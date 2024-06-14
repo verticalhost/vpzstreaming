9129172e5620f68e497aa2a0730d4c4e10fe2ca5
verticalhost <benoit@technoredac.ca>
2024-06-14 12:07:25 -0400

    TEST

--- app/api/webhooks/livekit/route.ts
+++ app/api/webhooks/livekit/route.ts
@@ -9,47 +9,36 @@ const receiver = new WebhookReceiver(
 );
 
 export async function POST(req: Request) {
-  try {
-    const body = await req.text();
-    const headerPayload = headers();
-    const authorization = headerPayload.get("Authorization");
+  const body = await req.text();
+  const headerPayload = headers();
+  const authorization = headerPayload.get("Authorization");
 
-    if (!authorization) {
-      return new Response("No authorization header", { status: 400 });
-    }
-
-    const event = receiver.receive(body, authorization);
-
-    if (event.event === "ingress_started") {
-      await db.stream.update({
-        where: {
-          ingressId: event.ingressInfo?.ingressId,
-        },
-        data: {
-          isLive: true,
-        },
-      });
-      return new Response("Ingress started event processed", { status: 200 });
-    }
-
-    if (event.event === "ingress_ended") {
-      await db.stream.update({
-        where: {
-          ingressId: event.ingressInfo?.ingressId,
-        },
-        data: {
-          isLive: false,
-        },
-      });
-      return new Response("Ingress ended event processed", { status: 200 });
-    }
-
-    // Default response for unhandled events
-    return new Response("Event not handled", { status: 400 });
-
-  } catch (error) {
-    // Handle the error (logging and returning a 500 response)
-    console.error("Error processing LiveKit webhook:", error); 
-    return new Response(`Error processing request: ${(error as Error).message}`, { status: 500 });
+  if (!authorization) {
+    return new Response("No authorization header", { status: 400 });
   }
+
+  const event = receiver.receive(body, authorization);
+
+  if (event.event === "ingress_started") {
+    await db.stream.update({
+      where: {
+        ingressId: event.ingressInfo?.ingressId,
+      },
+      data: {
+        isLive: true,
+      },
+    });
+  }
+
+  if (event.event === "ingress_ended") {
+    await db.stream.update({
+      where: {
+        ingressId: event.ingressInfo?.ingressId,
+      },
+      data: {
+        isLive: false,
+      },
+    });
+  }
+  return new Response("ok", { status: 200 });
 }
