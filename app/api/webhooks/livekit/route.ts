8a562a7ff4a9c09b9a0252107d53ee2c84b53744
verticalhost <benoit@technoredac.ca>
2024-06-18 17:43:52 -0400

    WEEBHOOK DISCORD

--- app/api/webhooks/livekit/route.ts
+++ app/api/webhooks/livekit/route.ts
@@ -1,5 +1,6 @@
 import { headers } from "next/headers";
 import { WebhookReceiver } from "livekit-server-sdk";
+import fetch from "node-fetch";
 
 import { db } from "@/lib/db";
 
@@ -7,6 +8,8 @@ const receiver = new WebhookReceiver(
   process.env.LIVEKIT_API_KEY!,
   process.env.LIVEKIT_API_SECRET!
 );
+
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1052278065834770552/7eQaisn4R1N_3RDpJgF9a2RO03mQ3MUsRzpjzva0Bs1tS1YqATx5NxDx7TD9RdOKXCp8";
 
 export async function POST(req: Request) {
   const body = await req.text();
@@ -28,6 +31,15 @@ export async function POST(req: Request) {
         isLive: true,
       },
     });
+
+    // Envoyer une notification à Discord via le webhook
+    await fetch(WEBHOOK_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        content: "🚨 New stream is just started on VPZ ! 🚨",
+      }),
+    });
   }
 
   if (event.event === "ingress_ended") {
@@ -40,5 +52,6 @@ export async function POST(req: Request) {
       },
     });
   }
+
   return new Response("ok", { status: 200 });
 }
