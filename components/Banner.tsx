f8b90faf10d3d8dab42a62212000c7502f800040
verticalhost <benoit@technoredac.ca>
2024-06-24 18:06:10 -0400

    refactor: Update Banner text to instruct users to refresh stream key

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner: React.FC = () => {
   return (
     <div className="bg-blue-600 text-white text-center py-4">
-      <p className="text-lg font-semibold">We are working on chat pop-up but it take time thanks.</p>
+      <p className="text-lg font-semibold">Please refresh your stream key to stream again.</p>
     </div>
   );
 };
